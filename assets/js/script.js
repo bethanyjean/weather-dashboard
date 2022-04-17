b85eb240d5a5ddf81198cd75d6abfc6161213e26
Bethany Jean <bethanyjeanbg@gmail.com>
2022-04-17 17:37:55 -0400

    worked on initial styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 var weatherKey = "&appid=dff97e676f389487cd5e3a16a8bd3463"
-var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?"
+var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="
 var apiUrlParam = "&exclude=hourly,minutely&units=imperial"
 var iconUrl = "http://openweathermap.org/img/wn/"
+var geoApi = "http://api.openweathermap.org/geo/1.0/direct?q="
+var city = document.querySelector("#city");
+var lat = 0
+var long = 0
+var savedCities = []
+
 
 var getWeather = function() {
-    var thisApi = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04" + apiUrlParam + weatherKey;
+    var thisApi = "https://api.openweathermap.org/data/2.5/onecall?lat" + lat +"&lon=" + long + apiUrlParam + weatherKey;
     fetch(thisApi)
         .then(function(response){
             if (response.ok) {
@@ -20,5 +26,7 @@ var getWeather = function() {
             alert("unable to connect");
         });
 };
+
+
 
 getWeather();
