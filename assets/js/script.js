cb71ce658e68149325f843e06a74e9a6e1b9f9bb
Bethany Griffin <bethanyjeanbg@gmail.com>
2022-05-27 22:58:07 -0400

    added api calls and event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,52 @@ var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="
 var apiUrlParam = "&exclude=hourly,minutely&units=imperial"
 var iconUrl = "http://openweathermap.org/img/wn/"
 var geoApi = "http://api.openweathermap.org/geo/1.0/direct?q="
-var city = document.querySelector("#city");
+var cityInputE1 = document.querySelector("#city");
+var cityFormE1 = document.querySelector("#city-form");
+var cityButtonsE1 = document.querySelector('#city-buttons');
 var lat = 0
 var long = 0
 var savedCities = []
+var currentWeatherE1 = document.querySelector('#current-weather')
+
+var getLatLong = function(city){
+   var thisApi =  geoApi + city + '&limit=1' + weatherKey;
+   fetch(thisApi)
+        .then(function(response){
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function(data){
+                    parseLatLong(data.items);
+                });
+            } else {
+                console.log("error: " + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            alert("unable to connect");
+        });
+}
+
+var parseLatLong = function(cityinfo) {
+    if (cityinfo.length === 0) {
+        currentWeatherE1.textContent = "No city found.";
+        return;
+      }
+      lat = cityinfo[0].lat;
+      long = cityinfo[0].lon;
+      getWeather();
+}
+
+var formSubmitHandler = function (event){
+    event.preventDefault();
+    var city = cityInputE1.value.trim();
+    getLatLong(city);
+}
+
 
 
 var getWeather = function() {
-    var thisApi = "https://api.openweathermap.org/data/2.5/onecall?lat" + lat +"&lon=" + long + apiUrlParam + weatherKey;
+    var thisApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + long + apiUrlParam + weatherKey;
     fetch(thisApi)
         .then(function(response){
             if (response.ok) {
@@ -29,4 +67,6 @@ var getWeather = function() {
 
 
 
-getWeather();
+//event listeners
+cityFormE1.addEventListener("submit", formSubmitHandler);
+cityButtonsE1.addEventListener("click", cityClickHandler);
